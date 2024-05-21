b056aa466f129d701c30a5b986f722370c971065
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-05-21 14:40:55 +0300

    add package.json to build
    Took 3 minutes

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://real-time-chat-five-eta.vercel.app",
+        target: "http://localhost:5000",
       },
     },
   },
