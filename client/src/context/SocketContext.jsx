94b945b50790ff3c2e6741fdb393d2568e158ab1
Vlad <114946628+m1liaron@users.noreply.github.com>
2024-05-21 16:05:33 +0300

    add url to request socket.io
    Took 3 minutes

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
     useEffect(() => {
         if(user){
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://real-time-chat-4ytc.onrender.com", {
                     query:{ userId: user._id }
             });
             setSocket(socket);
